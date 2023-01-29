55f0104929a907fe6b227c6fd90e357614d96ab0
Julian Yepe <yepejy@gmail.com>
2023-01-28 19:31:28 -0500

    Added tabbed page, navigation page, etc

--- TravelRecord/TravelRecord/MainPage.xaml.cs
+++ TravelRecord/TravelRecord/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace TravelRecord
 {
@@ -13,6 +14,26 @@ namespace TravelRecord
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void LoginButton_OnClicked(object sender, EventArgs e)
+        {
+            var userName = Username.Text;
+            var password = Password.Text;
+
+            if (String.IsNullOrEmpty(userName) || String.IsNullOrEmpty(password))
+            {
+               
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+
+        private void SignUpButton_OnClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
