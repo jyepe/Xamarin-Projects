049eb153ef1002f2befac3049dacdc0557826c63
Julian Yepe <yepejy@gmail.com>
2023-01-29 00:30:52 -0500

    Added bindings

--- TravelRecord/TravelRecord/History.xaml.cs
+++ TravelRecord/TravelRecord/History.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using SQLite;
+using TravelRecord.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,17 @@ namespace TravelRecord
         public History()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var con = new SQLiteConnection(App.databaseLocation);
+            con.CreateTable<Post>();
+            var posts = con.Table<Post>().ToList();
+            con.Close();
+            PostListView.ItemsSource = posts;
         }
     }
 }
