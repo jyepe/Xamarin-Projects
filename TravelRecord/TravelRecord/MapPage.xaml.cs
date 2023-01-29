27f087930d9f901bb26ec9c7559d1e3d1d00c770
Julian Yepe <yepejy@gmail.com>
2023-01-29 10:56:30 -0500

    Added map to map tab

--- TravelRecord/TravelRecord/MapPage.xaml.cs
+++ TravelRecord/TravelRecord/MapPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +15,38 @@ namespace TravelRecord
         public MapPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            GetLocation();
+        }
+
+        private async void GetLocation()
+        {
+            var status = await CheckPermission();
+
+            if (status == PermissionStatus.Granted)
+            {
+                var location = await Geolocation.GetLocationAsync();
+                Map.IsShowingUser = true;
+            }
+        }
+
+        private async Task<PermissionStatus> CheckPermission()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+
+            if (status == PermissionStatus.Granted)
+            {
+                return status;
+            }
+
+
+
+            status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            return status;
         }
     }
 }
